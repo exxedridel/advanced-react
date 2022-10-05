c3c18ea56460921ff032c9e71aa8235bdd3628d2
hevedrios <exxed.ridel@gmail.com>
2022-10-05 11:08:51 -0500

    Changing context from children, destructuring

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,13 +3,13 @@ import "../styles/global.scss";
 import ComponentDidUpdate from "./ComponentDidUpdate";
 import ComponentWillUnmount from "./ComponentWillUnmount";
 import DataFetcher from "./DataFetcher";
-import Header from "./Header"
-import Button from "./Button"
-import ThemeContext from "../themeContext"
+import Header from "./Header";
+import Button from "./Button";
+// import ThemeContext from "../themeContext";
 
 function App() {
   return (
-    <div>
+    <>
       {/* <DataFetcher url="https://swapi.dev/api/people/1">
         {({ data, loading }) => (
           loading ? 
@@ -18,19 +18,10 @@ function App() {
         )}
       </DataFetcher> */}
       <div>
-            <Header />
-            <ThemeContext.Consumer>
-                {theme => (
-                    <Button theme={theme} /> // theme setted by value setted in inde.js
-                )}
-            </ThemeContext.Consumer>
-            
-            <Button theme="dark"/> {/*dark theme hard coded*/}
-            <Button /> {/*if no theme provided will go default*/}
-            <Button theme="monkey"/> {/*if value not listed on possibles propsType warning will be thrown, and set to default*/}
-
-        </div>
-    </div>
+        <Header />
+        <Button />
+      </div>
+    </>
   );
 }
 
